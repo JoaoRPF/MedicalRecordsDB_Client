6a56251c33860a8e6eaa2ef59948e29c4df2beaa
JoaoRPF <joaof94@hotmail.com>
2015-12-01 23:27:05 +0000

    Menus a funcionar e chamadas remotas

--- src/domain/MedRecDBClient.java
+++ src/domain/MedRecDBClient.java
@@ -53,15 +53,20 @@ public class MedRecDBClient {
 	
 	public static void inicioNovoMenu(){
 		System.out.println("*****************");
-		System.out.println("0. Voltar");
+		System.out.println("0. Back");
 	}
 	
-	public static void menuDoctor(){
+	public static void menu(){
 		System.out.println("***** MEDICAL RECORDS DATABASE *****");
-        System.out.println("0. Sair");
-        System.out.println("1. Ver Registo de Paciente");
-        System.out.println("2. Mudar password");
-        System.out.print("Opcao: ");
+        System.out.println("0. Exit");
+        System.out.println("1. Check Patient Record");
+        System.out.println("2. Create Appointment");
+        System.out.println("3. Create User");
+        System.out.println("4. Create Patient");
+        System.out.println("5. Create Doctor");
+        System.out.println("6. Create Staff");
+        System.out.println("7. Change Password");
+        System.out.print("Option: ");
     }
 	
 	public static void main(String arg[]) throws Exception{ 
@@ -89,8 +94,10 @@ public class MedRecDBClient {
 		String patientName;
 		Scanner entradaScanner = new Scanner(System.in);
 		String type;
-		String newPassword;
 		String[] inputsNovaPassword;
+		String records = null;
+		String description;
+		String newUsername;
 		
 		while (true){
 			try{
@@ -98,38 +105,120 @@ public class MedRecDBClient {
 				username = nameAndPass[0];
 				password = nameAndPass[1];			
 				remote.loginInSystem(username, password);
-				type = remote.getUserType(username);
 				do{
 					try{
-						if (type.equals("doctor")){
-							menuDoctor();
-							patientName = new String();
-							opcao = entradaScanner.nextInt();
-							if (opcao == 0)
+						menu();
+						patientName = new String();
+						opcao = entradaScanner.nextInt();
+						if (opcao == 0)
+							break;
+						inicioNovoMenu();
+						
+						switch(opcao){
+						case 1: // CHECK PATIENT RECORD
+							nameAndPass = getCredentials();
+							if (nameAndPass == null)
 								break;
-							inicioNovoMenu();
-							
-							switch(opcao){
-							case 1:
-								nameAndPass = getCredentials();
-								if (nameAndPass == null)
-									break;
-								username = nameAndPass[0];
-								password = nameAndPass[1];
+							username = nameAndPass[0];
+							password = nameAndPass[1];
+							type = remote.getUserType(username);
+							if(type.equals("doctor")){
 								System.out.print("Patient Username: ");
 								patientName = entradaScanner.next();
 								if(patientName.equals("0"))
 									break;
 								System.out.println();
-								String records = remote.doctorViewRecord(username, password, patientName, false);
-								System.out.println(records);
-								break;
-								
-							case 2:
-								inputsNovaPassword = mudaPassword();
-								remote.changePassword(username, inputsNovaPassword[0], inputsNovaPassword[1], inputsNovaPassword[2]);
-								System.out.println("Password changed with success!");
+								records = remote.doctorViewRecord(username, password, patientName, false);
+							}else{
+								records = remote.viewRecord(username, password);
 							}
+							
+							System.out.println(records);
+							break;
+						
+						case 2: //CREATE APPOINTMENT
+							nameAndPass = getCredentials();
+							if (nameAndPass == null)
+								break;
+							username = nameAndPass[0];
+							password = nameAndPass[1];
+							System.out.print("Patient Username: ");
+							patientName = entradaScanner.next();
+							if(patientName.equals("0"))
+								break;
+							System.out.print("Description: ");
+							description = entradaScanner.next();
+							if(description.equals("0"))
+								break;						
+							remote.createAppointment(username, password, patientName, description);
+							break;
+							
+						case 3: //CREATE USER
+							System.out.print("New Username to be created: ");
+							newUsername = entradaScanner.next();
+							if (newUsername.equals("0"))
+								break;
+							String newPass = remote.createUser(username, password, newUsername);
+							System.out.println(newUsername + "'s Password: " + newPass);
+							break;
+							
+						case 4: //CREATE PATIENT
+							System.out.print("Username of the Patient: ");
+							newUsername = entradaScanner.next();
+							if (newUsername.equals("0"))
+								break;
+							System.out.print("First Name: ");
+							String first = entradaScanner.next();
+							if (first.equals("0"))
+								break;
+							System.out.print("Last Name: ");
+							String last = entradaScanner.next();
+							if (last.equals("0"))
+								break;
+							String address = entradaScanner.next();
+							if (address.equals("0"))
+								break;
+							remote.createPatient(username, password, false, newUsername, first, last, address);
+							break;
+							
+						case 5: //CREATE DOCTOR
+							nameAndPass = getCredentials();
+							if (nameAndPass == null)
+								break;
+							username = nameAndPass[0];
+							password = nameAndPass[1];
+							System.out.print("Username of the Doctor: ");
+							newUsername = entradaScanner.next();
+							if (newUsername.equals("0"))
+								break;
+							System.out.print("Specialty: ");
+							String spec = entradaScanner.next();
+							if (spec.equals("0"))
+								break;
+							remote.createDoctor(username, password, newUsername, spec);
+							break;
+							
+						case 6: //CREATE STAFF
+							nameAndPass = getCredentials();
+							if (nameAndPass == null)
+								break;
+							username = nameAndPass[0];
+							password = nameAndPass[1];
+							System.out.print("Username of the Staff Member: ");
+							newUsername = entradaScanner.next();
+							if (newUsername.equals("0"))
+								break;
+							System.out.print("Wage: ");
+							int wage = entradaScanner.nextInt();
+							if (wage == 0)
+								break;
+							remote.createStaff(username, password, newUsername, wage);
+							break;
+							
+						case 7: //CHANGE PASSWORD
+							inputsNovaPassword = mudaPassword();
+							remote.changePassword(username, inputsNovaPassword[0], inputsNovaPassword[1], inputsNovaPassword[2]);
+							System.out.println("Password changed with success!");
 						}
 					}
 					catch (Exception e){

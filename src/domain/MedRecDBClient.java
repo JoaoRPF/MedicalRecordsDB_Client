24b526fd679583305f2c162d166340d8f19ed351
ruidbras <rui_bras_@hotmail.com>
2015-12-03 20:37:19 +0000

    ssl stuff

--- src/domain/MedRecDBClient.java
+++ src/domain/MedRecDBClient.java
@@ -1,6 +1,10 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream.GetField;
+import java.net.URL;
 import java.rmi.ConnectException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -12,33 +16,42 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Scanner;
 
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
 import domain.MedRecDBInterface;
 import exception.CantLoginException;
+import exception.InvalidPasswordException;
+import exception.UsernameDoesntExistException;
 
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class MedRecDBClient {
-	
-	public static String[] getCredentials(){
+
+	public static String[] getCredentials() throws UsernameDoesntExistException, InvalidPasswordException {
 		String username = new String();
 		String password = new String();
 		Scanner entradaScanner = new Scanner(System.in);
 		System.out.println("***** LOGIN MEDICAL RECORDS DATABASE *****");
-		System.out.println("Insert your:");
+		System.out.println("Insert your");
 		System.out.print("Username: ");
 		username = entradaScanner.next();
-		if (username.equals("0"))
-			return null;
+
+		if (username.isEmpty())
+			throw new UsernameDoesntExistException();
 		System.out.print("Password: ");
+
 		password = entradaScanner.next();
-		if (password.equals("0"))
-			return null;
+
+		if (password.isEmpty())
+			throw new InvalidPasswordException();
 		String[] userAndPass = new String[2];
 		userAndPass[0] = username;
 		userAndPass[1] = password;
 		return userAndPass;
 	}
-	
-	public static String[] mudaPassword(){
+
+	public static String[] mudaPassword() {
 		String[] inputs = new String[3];
 		Scanner entradaScanner = new Scanner(System.in);
 		System.out.print("Actual Password: ");
@@ -48,45 +61,54 @@ public class MedRecDBClient {
 		System.out.print("Confirm your new Password: ");
 		inputs[2] = entradaScanner.next();
 		return inputs;
-		
+
 	}
-	
-	public static void inicioNovoMenu(){
+
+	public static void inicioNovoMenu() {
 		System.out.println("*****************");
 		System.out.println("0. Back");
 	}
-	
-	public static void menu(){
+
+	public static void menu() {
 		System.out.println("***** MEDICAL RECORDS DATABASE *****");
-        System.out.println("0. Exit");
-        System.out.println("1. Check Patient Record");
-        System.out.println("2. Create Appointment");
-        System.out.println("3. Create User");
-        System.out.println("4. Create Patient");
-        System.out.println("5. Create Doctor");
-        System.out.println("6. Create Staff");
-        System.out.println("7. Change Password");
-        System.out.print("Option: ");
-    }
-	
-	public static void main(String arg[]) throws Exception{ 
+		System.out.println("0. Exit");
+		System.out.println("1. Check Patient Record");
+		System.out.println("2. Create Appointment");
+		System.out.println("3. Create User");
+		System.out.println("4. Create Patient");
+		System.out.println("5. Create Doctor");
+		System.out.println("6. Create Staff");
+		System.out.println("7. Change Password");
+		System.out.print("Option: ");
+	}
+
+	public static void main(String arg[]) throws Exception {
 
 		MedRecDBInterface remote = null;
-		try {
-			Registry registry = LocateRegistry.getRegistry("localhost", 8095);
-			remote = (MedRecDBInterface)registry.lookup("MedicalRecordsDatabase");
-			System.out.println("Cliente comunica com server");
-		} catch (ConnectException e) {
-			throw new ConnectException("Cliente não encontra server");
-		}
-		
-		/*try{
-			remote.createUser("greg", "9363", "ZZZZZ");
-		}
-		catch(Exception e){
-			System.out.println(e.getMessage());
-		}*/
-		
+
+		// System.setProperty("java.security.policy","file:C:/Users/Rui/Documents/GitHub/MedicalRecordsDB_Client/src/domain/client.policy");
+		// System.setSecurityManager(new RMISecurityManager());
+
+		System.setProperty("javax.net.ssl.keyStore",
+				"C:/Users/Rui/Documents/GitHub/MedicalRecordsDB_Client/src/keystore");
+		System.setProperty("javax.net.ssl.keyStorePassword", "banana");
+		System.setProperty("javax.net.ssl.trustStore",
+				"C:/Users/Rui/Documents/GitHub/MedicalRecordsDB_Client/src/truststore");
+		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+
+		Registry registry = LocateRegistry.getRegistry("193.136.167.82", 9999, new SslRMIClientSocketFactory());
+
+		remote = (MedRecDBInterface) registry.lookup("MedicalRecordsDatabase");
+
+		/*
+		 * try { Registry registry =
+		 * LocateRegistry.getRegistry("193.136.167.82", 1099); remote =
+		 * (MedRecDBInterface)registry.lookup("MedicalRecordsDatabase");
+		 * System.out.println("Cliente comunica com server"); } catch
+		 * (ConnectException e) { throw new ConnectException(
+		 * "Cliente não encontra server"); }
+		 */
+
 		int opcao = 999;
 		String[] nameAndPass;
 		String username;
@@ -98,23 +120,23 @@ public class MedRecDBClient {
 		String records = null;
 		String description;
 		String newUsername;
-		
-		while (true){
-			try{
+
+		while (true) {
+			try {
 				nameAndPass = getCredentials();
 				username = nameAndPass[0];
-				password = nameAndPass[1];			
+				password = nameAndPass[1];
 				remote.loginInSystem(username, password);
-				do{
-					try{
+				do {
+					try {
 						menu();
 						patientName = new String();
 						opcao = entradaScanner.nextInt();
 						if (opcao == 0)
 							break;
 						inicioNovoMenu();
-						
-						switch(opcao){
+
+						switch (opcao) {
 						case 1: // CHECK PATIENT RECORD
 							nameAndPass = getCredentials();
 							if (nameAndPass == null)
@@ -122,21 +144,21 @@ public class MedRecDBClient {
 							username = nameAndPass[0];
 							password = nameAndPass[1];
 							type = remote.getUserType(username);
-							if(type.equals("doctor")){
+							if (type.equals("doctor")) {
 								System.out.print("Patient Username: ");
 								patientName = entradaScanner.next();
-								if(patientName.equals("0"))
+								if (patientName.equals("0"))
 									break;
 								System.out.println();
 								records = remote.doctorViewRecord(username, password, patientName, false);
-							}else{
+							} else {
 								records = remote.viewRecord(username, password);
 							}
-							
+
 							System.out.println(records);
 							break;
-						
-						case 2: //CREATE APPOINTMENT
+
+						case 2: // CREATE APPOINTMENT
 							nameAndPass = getCredentials();
 							if (nameAndPass == null)
 								break;
@@ -144,16 +166,16 @@ public class MedRecDBClient {
 							password = nameAndPass[1];
 							System.out.print("Patient Username: ");
 							patientName = entradaScanner.next();
-							if(patientName.equals("0"))
+							if (patientName.equals("0"))
 								break;
 							System.out.print("Description: ");
 							description = entradaScanner.next();
-							if(description.equals("0"))
-								break;						
+							if (description.equals("0"))
+								break;
 							remote.createAppointment(username, password, patientName, description);
 							break;
-							
-						case 3: //CREATE USER
+
+						case 3: // CREATE USER
 							System.out.print("New Username to be created: ");
 							newUsername = entradaScanner.next();
 							if (newUsername.equals("0"))
@@ -161,8 +183,8 @@ public class MedRecDBClient {
 							String newPass = remote.createUser(username, password, newUsername);
 							System.out.println(newUsername + "'s Password: " + newPass);
 							break;
-							
-						case 4: //CREATE PATIENT
+
+						case 4: // CREATE PATIENT
 							System.out.print("Username of the Patient: ");
 							newUsername = entradaScanner.next();
 							if (newUsername.equals("0"))
@@ -180,8 +202,8 @@ public class MedRecDBClient {
 								break;
 							remote.createPatient(username, password, false, newUsername, first, last, address);
 							break;
-							
-						case 5: //CREATE DOCTOR
+
+						case 5: // CREATE DOCTOR
 							nameAndPass = getCredentials();
 							if (nameAndPass == null)
 								break;
@@ -197,8 +219,8 @@ public class MedRecDBClient {
 								break;
 							remote.createDoctor(username, password, newUsername, spec);
 							break;
-							
-						case 6: //CREATE STAFF
+
+						case 6: // CREATE STAFF
 							nameAndPass = getCredentials();
 							if (nameAndPass == null)
 								break;
@@ -214,21 +236,20 @@ public class MedRecDBClient {
 								break;
 							remote.createStaff(username, password, newUsername, wage);
 							break;
-							
-						case 7: //CHANGE PASSWORD
+
+						case 7: // CHANGE PASSWORD
 							inputsNovaPassword = mudaPassword();
-							remote.changePassword(username, inputsNovaPassword[0], inputsNovaPassword[1], inputsNovaPassword[2]);
+							remote.changePassword(username, inputsNovaPassword[0], inputsNovaPassword[1],
+									inputsNovaPassword[2]);
 							System.out.println("Password changed with success!");
 						}
-					}
-					catch (Exception e){
+					} catch (Exception e) {
 						System.out.println(e.getMessage());
 						if (e.toString().contains("CantLogin"))
-							opcao=0;
+							opcao = 0;
 					}
-				} while(opcao != 0);
-			} 
-			catch(CantLoginException e){
+				} while (opcao != 0);
+			} catch (CantLoginException e) {
 				System.out.println(e.getMessage());
 			}
 		}
